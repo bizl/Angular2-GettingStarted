9552f883b3b5230568c0c346b27d82c9e0932aa0
EMEA\lafonb <bime.lafon@burrows.yr.com>
2017-04-27 17:49:49 +0100

    .. .

---
+++ APM - Start/app/ta2-head.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+	selector: 'tatuness-head',
+	template: `
+	<div><h1> </h1>
+	<div>see nose! </div>
+	</div>
+	`
+})
+export class TatusComponent { }
+ 
