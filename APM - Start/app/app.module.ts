daf0668b74c282a538f889e4eb949433f744c94d
EMEA\lafonb <bime.lafon@burrows.yr.com>
2017-04-27 17:49:34 +0100

    testing ta2's component - not quite working

--- APM - Start/app/app.module.ts
+++ APM - Start/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+ 
+import { TatusComponent }  from './ta2-head.component';
 import { AppComponent }  from './app.component';
 
 @NgModule({
   imports: [ BrowserModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, TatusComponent ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
