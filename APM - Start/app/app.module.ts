2e6fd5e69cbba61186b37524eb2077b856b1ccea
EMEA\lafonb <bime.lafon@burrows.yr.com>
2017-04-27 17:57:48 +0100

    ta2's components - lets see

--- APM - Start/app/app.module.ts
+++ APM - Start/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
  
-import { TatusComponent }  from './ta2-head.component';
+import { TatusComponent }  from './tatuhead.component';
 import { AppComponent }  from './app.component';
 
 @NgModule({
---
+++ APM - Start/app/tatuhead.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'pm-tatuness',
+    template: `
+        <h1>ta2: already angular</h1>
+    `
+})
+export class TatusComponent { }
